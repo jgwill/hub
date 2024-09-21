f02b0ef852af9adca5efbef6181beac3eb84b093
JGWill <jgi@jgwill.com>
2024-09-21 08:41:08 -0400

    packaging

--- src/jgwillhub2/jgwillhub/prompts.py
+++ src/jgwillhub2/jgwillhub/prompts.py
@@ -1,4 +1,5 @@
 def pull(tool_hub_tag: str) -> str:
     # Implementation to pull prompts from 'langchainhub' based on the tool_hub_tag
     # Add your code here
+    print("FUTURE: Pulling prompts from 'langchainhub' based on the tool_hub_tag")
     pass
